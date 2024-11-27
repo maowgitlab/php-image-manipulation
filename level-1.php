3cebc4793f81e29d0f9513e9723c7874ec3a58d8
maowgit-lab <robianoor2002@gmail.com>
2024-11-27 22:34:51 +0800

    implement GD library

---
+++ level-1.php
@@ -0,0 +1,43 @@
+<?php
+function resizeImage($file, $width, $height) {
+    list($originalWidth, $originalHeight) = getimagesize($file);
+    $sourceImage = imagecreatefromjpeg($file);
+
+    $resizedImage = imagecreatetruecolor($width, $height);
+
+    imagecopyresampled($resizedImage, $sourceImage, 0, 0, 0, 0, $width, $height, $originalWidth, $originalHeight);
+
+    return $resizedImage;
+}
+
+function cropImage($file, $cropWidth, $cropHeight) {
+    list($originalWidth, $originalHeight) = getimagesize($file);
+    $sourceImage = imagecreatefromjpeg($file);
+
+    $startX = ($originalWidth - $cropWidth) / 2;
+    $startY = ($originalHeight - $cropHeight) / 2;
+
+    $croppedImage = imagecreatetruecolor($cropWidth, $cropHeight);
+
+    imagecopyresampled($croppedImage, $sourceImage, 0, 0, $startX, $startY, $cropWidth, $cropHeight, $cropWidth, $cropHeight);
+
+    return $croppedImage;
+}
+
+$file = 'pexels-peter-fazekas-1089451.jpg';
+
+$newWidth = 800;
+$newHeight = 600;
+
+$cropWidth = 400;
+$cropHeight = 300;
+
+$resizedImage = resizeImage($file, $newWidth, $newHeight);
+imagejpeg($resizedImage, 'resized_image.jpg');
+
+$croppedImage = cropImage($file, $cropWidth, $cropHeight);
+imagejpeg($croppedImage, 'cropped_image.jpg');
+
+imagedestroy($resizedImage);
+imagedestroy($croppedImage);
+?>
